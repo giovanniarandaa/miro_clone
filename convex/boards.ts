2a5266b3efcd6d43074253485c317d2a19939e99
Giovanni Aranda <giovanni.arandaa@gmail.com>
2024-06-24 15:28:43 -0600

    Board List

--- convex/boards.ts
+++ convex/boards.ts
@@ -1,41 +1,23 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { query } from "./_generated/server";
 
-const images = [
-  "/placeholders/1.jpg",
-  "/placeholders/2.jpg",
-  "/placeholders/3.jpg",
-  "/placeholders/4.jpg",
-  "/placeholders/5.jpg",
-  "/placeholders/6.jpg",
-  "/placeholders/7.jpg",
-  "/placeholders/8.jpg",
-  "/placeholders/9.jpg",
-  "/placeholders/10.jpg",
-]
-
-export const create = mutation({
+export const get = query({
   args: {
-    orgId: v.string(),
-    title: v.string()
+    orgId: v.string()
   },
   handler: async (ctx, args) => {
-    const identify = await ctx.auth.getUserIdentity()
+    const identity = await ctx.auth.getUserIdentity()
 
-    if (!identify) {
+    if (!identity) {
       throw new Error('Unauthorized')
     }
 
-    const randomImage = images[Math.floor(Math.random() * images.length)]
+    const boards = await ctx.db
+      .query('boards')
+      .withIndex('by_org', (q) => q.eq('orgId', args.orgId))
+      .order('desc')
+      .collect()
 
-    const board = await ctx.db.insert('boards', {
-      title: args.title,
-      orgId: args.orgId,
-      authorId: identify.subject,
-      authorName: identify.name!,
-      imageUrl: randomImage,
-    })
-
-    return board
+    return boards
   }
 })
