a903b800bbd41c9ff2508ab2e379025abbeed2a0
Giovanni Aranda <giovanni.arandaa@gmail.com>
2024-06-25 12:28:34 -0600

    Card Actions

--- app/(dashboard)/_components/invite-button.tsx
+++ app/(dashboard)/_components/invite-button.tsx
@@ -2,7 +2,6 @@ import { Button } from '@/components/ui/button'
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
 import { OrganizationProfile } from '@clerk/nextjs'
 import { Plus } from 'lucide-react'
-import React from 'react'
 
 export const InviteButton = () => {
   return (
